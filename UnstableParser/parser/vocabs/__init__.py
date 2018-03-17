5393e813e5bc4d5d966814e6cc827e10a44491b9
evap <portelance.eva@gmail.com>
2018-03-17 14:07:44 -0700

    fixed python3 convert bug

--- UnstableParser/parser/vocabs/__init__.py
+++ UnstableParser/parser/vocabs/__init__.py
@@ -1,10 +1,10 @@
-from index_vocab import IndexVocab, DepVocab, HeadVocab
-from pretrained_vocab import PretrainedVocab
-from token_vocab import TokenVocab, WordVocab, LemmaVocab, TagVocab, XTagVocab, RelVocab
-from subtoken_vocab import SubtokenVocab, CharVocab
-from ngram_vocab import NgramVocab
-from multivocab import Multivocab
-from ngram_multivocab import NgramMultivocab
+from vocabs.index_vocab import IndexVocab, DepVocab, HeadVocab
+from vocabs.pretrained_vocab import PretrainedVocab
+from vocabs.token_vocab import TokenVocab, WordVocab, LemmaVocab, TagVocab, XTagVocab, RelVocab
+from vocabs.subtoken_vocab import SubtokenVocab, CharVocab
+from vocabs.ngram_vocab import NgramVocab
+from vocabs.multivocab import Multivocab
+from vocabs.ngram_multivocab import NgramMultivocab
 
 __all__ = [
   'DepVocab',

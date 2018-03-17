07ba20d68c1dcf15e623d0f231f14c622dd4260a
evap <portelance.eva@gmail.com>
2018-03-17 14:02:23 -0700

    fixed bug for python3

--- UnstableParser/parser/__init__.py
+++ UnstableParser/parser/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from configurable import Configurable
-from bucket import Bucket
-from multibucket import Multibucket
-from network import Network
+from parser.configurable import Configurable
+from parser.bucket import Bucket
+from parser.multibucket import Multibucket
+from parser.network import Network
